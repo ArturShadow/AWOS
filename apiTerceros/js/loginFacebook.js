8eb03be6f5a715e277b1b8b3f4765505c63c8223
Arturo Loya <41841053+ArturShadow@users.noreply.github.com>
2022-08-05 12:59:15 -0600

    ApiTerceros

---
+++ apiTerceros/js/loginFacebook.js
@@ -0,0 +1,65 @@
+var persona = { userID: "", name: "", accessToken: "", picture: "", email: "" };
+
+function login() {
+
+    FB.login(function (response) {
+        if (response.status == "connected") {
+
+
+
+            persona.userID = response.authResponse.userID;
+
+            persona.accessToken = response.authResponse.accessToken;
+
+            //5
+
+            FB.api('/me?fields=id,name,first_name,last_name,email,picture.type(large)', function (userData) {
+                console.log(userData);
+                
+
+                persona.name = userData.name;
+                persona.email = userData.email;
+                persona.picture = userData.picture.data.url;
+
+                $.ajax({
+                    url:'login.php',
+                    method: 'POST',
+                    data:persona,
+                    dataType:'text',
+                    succes: function(serverResponse){
+                        if(serverResponse=='success'){
+                            window.location = 'index.php';
+                        }
+                    }
+                })
+            })
+
+        }
+        
+    }, { scope: 'public_profile, email' });
+}
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '3178375025763539',
+            autoLogAppEvent: true,
+            xfbml: true,
+            version: 'v14.0'
+        });
+    
+        FB.AppEvents.logPageView();
+    
+    };
+    
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+
+
--- apiTerceros/login.php
+++ apiTerceros/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,14 +12,15 @@
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
         crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div class="container" style="margin-top: 100px;">
         <div class="row justify-content-center">
             <div class="col-md-6 col- offset-1" aling="center">
                 <img src="./img/logo.png" alt="" srcset=""><br><br><br>
                 <form action="">
-                    <input type="email" name="email" placeholder="Email" id="" class="form-control">
-                    <input type="password" name="password" id="" placeholder="Password" class="form-control">
+                    <input type="email" name="email" placeholder="Email" id="email-text" class="form-control">
+                    <input type="password" name="password" id="pass-text" placeholder="Password" class="form-control">
                     <button type="submit">Log in</button>
                     <input type="button" name="btn-facebook" value="Login with Facebook" onclick="login()">
                 </form>
@@ -26,7 +28,12 @@
 
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+
+
+    <script src="./js/loginFacebook.js"></script>
 </body>
+
 </html>
